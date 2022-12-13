4484b362f93d5a55ce49aa411cc107baabb22b6f
Joshua Paino <joshua.paino@gmail.com>
2022-12-13 10:31:54 -0600

    Refactor tests and ran them independently

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,2 +1,3 @@
+import React from "react";
 // Adding Blank Appointment so the test is rendered here
-export const Appointment = () => "Ashley";
+export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -7,12 +7,25 @@ import { Appointment } from "../src/Appointment";
 import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
-  it("renders the customer first name", () => {
-    const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.appendChild(container);
+  let container;
+  let customer;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+  const render = (component) =>
     act(() => ReactDOM.createRoot(container).render(component));
+
+  it("renders the customer first name", () => {
+    customer = { firstName: "Ashley" };
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it("renders another customer first name", () => {
+    customer = { firstName: "Jordan" };
+    render(<Appointment customer={customer} />);
+    expect(document.body.textContent).toContain("Jordan");
   });
 });
